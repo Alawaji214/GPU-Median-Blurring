10248defd2dc2591549976d760ef5a0458c45574
Abdullah Barnawi <aabarnawi@DESKTOP-N28GE6D>
2023-12-13 14:36:03 +0300

    add workers gangs and threads

--- c/openacc/median_filter.cpp
+++ c/openacc/median_filter.cpp
@@ -131,7 +131,13 @@ void applyMedianFilter4(uchar *input, uchar *output, int rows, int cols, int ker
     uchar neighbors[windowSize];
     // #pragma acc data copyin(input[0:rows*cols]) copyout(output[0:rows*cols])
     {
-#pragma acc parallel loop collapse(2) copyin(input[0 : rows * cols]) copyout(output[0 : rows * cols]) private(neighbors)
+#pragma acc parallel loop collapse(2) \
+    copyin(input[0 : rows * cols]) \
+    copyout(output[0 : rows * cols]) \
+    private(neighbors) \
+    num_gangs(132) /* 4 times the number of SMs to keep the GPU busy */ \
+    num_workers(32) /* One worker per warp */ \
+    vector_length(32) /* Equal to the warp size */
         for (int y = edge; y < rows - edge; y++)
         {
             for (int x = edge; x < cols - edge; x++)
