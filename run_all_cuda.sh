5174b3ea7cbc40f9d7e40cec37c280c5dca59095
m.alawaji <moh214011@gmail.com>
2023-12-15 20:30:28 +0300

    add a script to nsys all the images

---
+++ run_all_cuda.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the CUDA program
+program="image_cuda_median"
+
+# Loop over the 5 noisy images
+for i in {1..5}
+do
+  # Define the input and output image names
+  input_image="noisy_image${i}.jpg"
+
+  echo "-------------------------"
+  echo "Processing ${input_image}"
+
+  # Run nsys profile
+  nsys profile --stats=true -o profile${i} ./${program} ${input_image}  2>&1 
+
+  echo "-------------------------"
+done
